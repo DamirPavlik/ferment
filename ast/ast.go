3df7c8e069df00f4a7429eee79d0744585219906
DamirPavlik <damir.pavlik@gmail.com>
2025-03-16 14:53:47 +0100

    feat: add abstarct syntax tree for dsl let statement

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import "ferment/lexer"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type Identifier struct {
+	Token lexer.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.String() }
+
+type IntegerLiteral struct {
+	Token lexer.Token
+	Value string
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.String() }
+
+type LetStatement struct {
+	Token lexer.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.String() }
