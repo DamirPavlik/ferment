a538d1e1873eaa04218dc4745120b1111dd347b6
DamirPavlik <damir.pavlik@gmail.com>
2025-03-25 22:19:57 +0100

    feat: add parse statements

--- parser/parser.go
+++ parser/parser.go
@@ -27,19 +27,30 @@ func NewParser(l *lexer.Lexer) *Parser {
 }
 
 func (p *Parser) nextToken() {
-	pos, tok, _ := p.l.Lex()
-
-	fmt.Printf("nextToken - pos: %+v, token: %v (%s)\n", pos, tok, tok.String())
+	_, tok, _ := p.l.Lex()
 
 	p.curToken = p.peekToken
 	p.peekToken = tok
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken {
 	case lexer.LET:
 		return p.parseLetStatement()
+	case lexer.RETURN:
+		return p.parseReturnStatement()
+	case lexer.IF:
+		return p.parseIfStatement()
+	case lexer.WHILE:
+		return p.parseWhileStatement()
+	case lexer.PRINT:
+		return p.parsePrintStatement()
 	default:
+		p.nextToken()
 		return nil
 	}
 }
@@ -77,7 +88,124 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	stmt.Value = p.parseExpression()
+
+	if p.peekToken == lexer.SEMICOLON {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseIfStatement() *ast.IfStatement {
+	stmt := &ast.IfStatement{Token: p.curToken}
+
+	if p.peekToken != lexer.LPAREN {
+		p.errors = append(p.errors, "expected '(' after AKO")
+		return nil
+	}
+
+	p.nextToken()
+	p.nextToken()
+
+	stmt.Condition = p.parseExpression()
+
+	if p.peekToken != lexer.RPAREN {
+		p.errors = append(p.errors, "expected ')' after condition")
+		return nil
+	}
+
+	p.nextToken()
+	p.nextToken()
+
+	if p.curToken != lexer.LBRACE {
+		p.errors = append(p.errors, "expected '{' after condition")
+		return nil
+	}
+
+	stmt.Consequence = p.parseBlockStatement()
+
+	if p.peekToken == lexer.ELSE {
+		p.nextToken()
+		p.nextToken()
+		stmt.Alternative = p.parseBlockStatement()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parsePrintStatement() *ast.PrintStatement {
+	stmt := &ast.PrintStatement{Token: p.curToken}
+
+	p.nextToken()
+	stmt.Value = p.parseExpression()
+
+	if p.peekToken == lexer.SEMICOLON {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for p.curToken != lexer.RBRACE && p.curToken != lexer.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return block
+}
+
+func (p *Parser) parseWhileStatement() *ast.WhileStatement {
+	stmt := &ast.WhileStatement{Token: p.curToken}
+
+	p.nextToken()
+	stmt.Condition = p.parseExpression()
+
+	if p.curToken != lexer.LBRACE {
+		p.errors = append(p.errors, "expected '{' after condition")
+		return nil
+	}
+
+	stmt.Body = p.parseBlockStatement()
+	return stmt
+}
+
 func (p *Parser) parseExpression() ast.Expression {
+	left := p.parsePrimary()
+
+	for p.peekToken == lexer.PLUS || p.peekToken == lexer.MINUS || p.peekToken == lexer.ASTERISK || p.peekToken == lexer.SLASH || p.peekToken == lexer.GT || p.peekToken == lexer.LT {
+		p.nextToken()
+		operator := p.curToken
+
+		p.nextToken()
+		right := p.parsePrimary()
+
+		left = &ast.InfixExpression{
+			Token:    operator,
+			Left:     left,
+			Operator: operator.String(),
+			Right:    right,
+		}
+	}
+
+	return left
+}
+
+func (p *Parser) parsePrimary() ast.Expression {
 	switch p.curToken {
 	case lexer.IDENT:
 		return &ast.Identifier{
@@ -89,6 +217,20 @@ func (p *Parser) parseExpression() ast.Expression {
 			Token: p.curToken,
 			Value: p.curToken.String(),
 		}
+	case lexer.STRING:
+		return &ast.StringLiteral{
+			Token: p.curToken,
+			Value: p.curToken.String(),
+		}
+	case lexer.LPAREN:
+		p.nextToken()
+		exp := p.parseExpression()
+
+		if p.curToken != lexer.RPAREN {
+			p.errors = append(p.errors, "expected closing )")
+		}
+
+		return exp
 	}
 
 	return nil
@@ -99,8 +241,6 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program.Statements = []ast.Statement{}
 
 	for p.curToken != lexer.EOF {
-		fmt.Printf("ParseProgram loop - curToken: %v (%s)\n", p.curToken, p.curToken.String())
-
 		stmt := p.parseStatement()
 
 		if stmt != nil {
